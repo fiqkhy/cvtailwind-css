4a6aa6a653e05deb7e46563473344411309f80e5
fiqkhy <fiky.sandy.fs@gmail.com>
2023-08-11 09:58:36 +0700

    update darkmode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -37,13 +37,23 @@ window.addEventListener('click', function (e) {
 });
 
 // Darkmode toggle
-const darkToggle = document.querySelector('dark-toggle');
+const darkToggle = document.querySelector('#dark-toggle');
 const html = document.querySelector('html');
 
 darkToggle.addEventListener('click', function() {
  if (darkToggle.checked) {
     html.classList.add('dark');
+    localStorage.theme = 'dark';
  } else {
     html.classList.remove('dark');
+    localStorage.theme = 'light';
  }
 });
+
+
+//Pindah Posisi Toggle sesua mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
